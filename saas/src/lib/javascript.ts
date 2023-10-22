c8c6f992660fa7a59e50786cc72fc6bbfd1fb933
Nazrul7711 <nazrul7711@gmail.com>
2023-10-22 16:56:10 +0530

    dashboard page added

---
+++ saas/src/lib/javascript.ts
@@ -0,0 +1,48 @@
+let values = [0, 1, 5, 10, 15];
+values.sort((a, b) => b - a);
+console.log(values); // 0,1,10,15,5
+
+/*
+if u have newArr = [...arr] will change in newArr cause change in arr?
+
+what about newArr = Array.from(arr) will this cause change?
+
+what is array destructuring and spreading a array?
+
+Objects 
+if u have a object and a properety name how will u access it with bracket notation and how will u access name a property ?
+
+Array
+Array.from(array like things) what are those things can u array from objects
+
+difference between Array.of(1,2,3,4) and Array.of([1,2,3,4])?
+
+how will u find out if an object is an array?
+
+create array with keys and values as key,value of a list [1,2,3,4]
+create map with keys and values as key,value of a list [1,2,3,4]
+create object with keys and values as key,value of a list [1,2,3,4]
+
+what arr.keys() arr.values() arr.entries() will give ?
+
+[1,2,3].toString() will give what?
+
+let number = 1234567.89 number.toLocaleString(); will give me what?
+
+[1,2,4].join("|") ?
+
+if u have a array and u want to perform all operations related to queue and stack what are the methods?
+
+how shift and unshift works on js array?
+
+[0, 1, 5, 10, 15] will be sorted in what order and how to correct it?
+
+
+
+
+
+
+
+
+
+*/
