0ca590794090d70d9dbe424fd217f1a2c089d223
Nazrul7711 <nazrul7711@gmail.com>
2023-10-23 16:49:55 +0530

    modal feature added with background blur

--- saas/src/lib/javascript.ts
+++ saas/src/lib/javascript.ts
@@ -1,6 +1,4 @@
-let values = [0, 1, 5, 10, 15];
-values.sort((a, b) => b - a);
-console.log(values); // 0,1,10,15,5
+
 
 /*
 if u have newArr = [...arr] will change in newArr cause change in arr?
@@ -36,6 +34,36 @@ if u have a array and u want to perform all operations related to queue and stac
 how shift and unshift works on js array?
 
 [0, 1, 5, 10, 15] will be sorted in what order and how to correct it?
+
+arr.concat(1,2,[99,88]) will do what?
+
+how arr.splice is used for a item removal , addtion, and replacement?
+
+how arr.indexOf(4) lastIndexOf and includes do?
+and what indexOf(4,4) what will this do?
+
+hey arr.find and arr.findIndex will do what and in the function(ele=>something) in this function what are the elements of the paramter?
+
+diff between arr.every,some,map,forEach,filter,reduce,reduceRight
+
+map add delete get values and see if a map has a value as per  a specific key?
+
+how to see if a key is in object?
+
+difference between map and object?
+
+how u will find keys and values and entries of object and map?
+
+what is common between set and map?
+
+
+
+
+
+
+
+
+
 
 
 
